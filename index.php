c0e749befcfe5884360235b3baade3a0f8314c06
hukl <contact@smyck.org>
2023-05-12 06:26:11 +0000

    Hello world of themes

---
+++ index.php
@@ -0,0 +1,14 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Glitter Brains Podcast</title>
+    <link rel="stylesheet" href="css/bootstrap-grid.min.css">
+    <link rel="stylesheet" href="fonts/inter.css">
+    <link rel="stylesheet" href="css/style.css">
+  </head>
+  <body>
+    <h1>Hello World</h1>
+  </body>
+</html>

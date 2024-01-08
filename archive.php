65de8c7eb0870759eb562f11e099d01bc371553a
Letty <git@letty.io>
2024-01-08 13:48:35 +0100

    feat: basic archive site for episodes #1

--- archive.php
+++ archive.php
@@ -2,8 +2,17 @@
     <div class="container">
       <div class="row">
         <div class="col-sm-2"></div>
+
         <div class="col-sm-6">
-            
+        <?php foreach (\Podlove\get_podcast()->episodes() as $episode): ?>
+          <div class="gbr-archive-podcast-episode">
+            <a href="<?php echo $episode->url() ?>"><h4><?php echo $episode->title() ?></h4></a>
+            <div class="gbr-archive-podcast-episode">
+              <span><?php echo $episode->publicationDate()->format('d.m.Y') ?> - <?php echo $episode->duration() ?></span>
+            </div>
+            <p><?php echo $episode->summary() ?></p>
+          </div>
+        <?php endforeach ?>
         </div>
 
         <div class="col-sm-4">
